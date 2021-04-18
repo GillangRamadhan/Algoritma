8472652dc58311bb38b334a7417aec6dc5018e7a
Gilang Ramadhan <80237133+GillangRamadhan@users.noreply.github.com>
2021-04-18 22:48:06 +0700

    Add files via upload

---
+++ algortma/ALGORTMA.java
@@ -0,0 +1,17 @@
+
+package algortma;
+
+/**
+ *
+ * @author Gilang Ramadhan
+ */
+public class ALGORTMA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ algortma/NIM2.java
@@ -0,0 +1,20 @@
+package algortma;
+import java.util.*;
+public class NIM2 {
+    public static void main(String[] args) {
+        String nimScan = "";
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Masukkan NIM anda : ");
+        nimScan = scan.next();
+
+        NIM nim = new NIM(nimScan);
+
+        System.out.println("NIM : " + nimScan);
+        System.out.println("Jenjang Pendidikan : " + nim.jenjangPendidikan());
+        System.out.println("Angkatan : " + nim.angkatan());
+        System.out.println("Fakultas : " + nim.fakultas());
+        System.out.println("Prodi : " + nim.prodi());
+        System.out.println("Jenis Kelamin : " + nim.jenisKelamin());
+        System.out.println("No Unik : " + nim.noUnik());
+    }
+}
