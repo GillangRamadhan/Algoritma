7d7cdb80e171a94ca31d00811e9226cd59797c01
Gilang Ramadhan <80237133+GillangRamadhan@users.noreply.github.com>
2021-06-13 16:19:48 +0700

    Add files via upload

---
+++ Table/RunExampleTable1.java
@@ -0,0 +1,18 @@
+package com.company.gui.Table;
+
+import javax.swing.*;
+
+public class RunExampleTable1 {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ignored) {
+        }
+        JFrame jframe = new JFrame("Data Array");
+        jframe.setContentPane(new ExampleTable1().getRootPanel());
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.setSize(500,400);
+        jframe.setLocationRelativeTo(null);
+        jframe.setVisible(true);1111
+    }
+}
